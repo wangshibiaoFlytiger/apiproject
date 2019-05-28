209503072f3b40f6a61ac8e706852398259b3618
wangshibiao <55@qq,com>
2019-05-28 21:00:46 +0800

    feat(apiproject): 测试xml解析

---
+++ test/xml_test.go
@@ -0,0 +1,32 @@
+package test
+
+import (
+	"fmt"
+	"github.com/beevik/etree"
+	"testing"
+)
+
+/**
+测试xml解析
+*/
+func TestParseXml(t *testing.T) {
+	document := etree.NewDocument()
+	if err := document.ReadFromFile("./xml_demo.xml"); err != nil {
+		panic(err)
+	}
+
+	root := document.SelectElement("bookstore")
+	fmt.Println("ROOT element:", root.Tag)
+
+	for _, book := range root.SelectElements("book") {
+		fmt.Println("CHILD element:", book.Tag)
+		if title := book.SelectElement("title"); title != nil {
+			lang := title.SelectAttrValue("lang", "unknown")
+			fmt.Printf("  TITLE: %s (%s)\n", title.Text(), lang)
+		}
+		for _, attr := range book.Attr {
+			fmt.Printf("  ATTR: %s=%s\n", attr.Key, attr.Value)
+		}
+	}
+
+}
