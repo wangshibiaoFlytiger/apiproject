6f31461f19383494da95df73d89a9db278d815b3
wangshibiao <55@qq,com>
2019-05-31 18:11:30 +0800

    feat(apiproject): 日志中间件由局部配置为全局

--- router/router.go
+++ router/router.go
@@ -46,12 +46,11 @@ func Init() *gin.Engine {
 
 	//支持跨域
 	engine.Use(cors.Default())
+	//全局配置api日志访问中间件
+	engine.Use(middleware.ApiLogMiddleware)
 
 	//视频相关接口
 	videoGroup := engine.Group("/api/video")
-	//配置api日志访问中间件
-	videoGroup.Use(middleware.ApiLogMiddleware)
-
 	videoGroup.GET("/findList", c_video.FindVideoList)
 	videoGroup.GET("/findVideoByWhere", c_video.FindVideoByWhere)
 	videoGroup.GET("/addVideo", c_video.AddVideo)
