73b76c89b2377b82f3c240c1bdcfbfd9755f6647
wangshibiao <55@qq,com>
2019-06-26 11:06:29 +0800

    refactor(apiproject): 优化ID生成器雪花算法, 减小高并发下ID生成重复的问题

--- util/id_util.go
+++ util/id_util.go
@@ -1,13 +1,17 @@
 package util
 
-import "github.com/bwmarrin/snowflake"
+import (
+	"github.com/Pallinder/go-randomdata"
+	"github.com/bwmarrin/snowflake"
+)
 
 /**
 生成分布式唯一ID: 整数,自增,且不连续
 */
 func GenUniqueId() snowflake.ID {
-	// Create a new Node with a Node number of 1
-	node, err := snowflake.NewNode(1)
+	// Create a new Node with a Node number of 10000
+	var nodeNumber int64 = 900
+	node, err := snowflake.NewNode(nodeNumber + int64(randomdata.Number(99)))
 	if err != nil {
 		panic(err)
 	}
