53b903e85bcee011742b3d8b5f22357fa89fc774
wangshibiao <55@qq,com>
2019-06-27 10:09:49 +0800

    feat(apiproject): 补充类型转换函数

--- util/type_convert_util.go
+++ util/type_convert_util.go
@@ -1,6 +1,8 @@
 package util
 
-import "strconv"
+import (
+	"strconv"
+)
 
 func Int64ToInt(int64Value int64) int {
 	strValue := strconv.FormatInt(int64Value, 10)
@@ -14,4 +16,43 @@ func IntToInt64(intValue int) int64 {
 	int64Value, _ := strconv.ParseInt(strValue, 10, 64)
 
 	return int64Value
+}
+
+func IntToStr(intValue int) string {
+	return strconv.Itoa(intValue)
+}
+
+func Int64ToStr(int64Value int64) string {
+	return strconv.FormatInt(int64Value, 10)
+}
+
+func StrToInt(stringValue string) (int, error) {
+	intValue, error := strconv.Atoi(stringValue)
+	if error != nil {
+		return 0, error
+	}
+
+	return intValue, nil
+}
+
+func StrToInt64(stringValue string) (int64, error) {
+	int64Value, error := strconv.ParseInt(stringValue, 10, 64)
+	if error != nil {
+		return 0, error
+	}
+
+	return int64Value, nil
+}
+
+func Float64ToStr(float64Value float64) string {
+	return strconv.FormatFloat(float64Value, 'E', -1, 64)
+}
+
+func StrToFloat64(stringValue string) (float64, error) {
+	float64Value, error := strconv.ParseFloat(stringValue, 64)
+	if error != nil {
+		return 0, error
+	}
+
+	return float64Value, nil
 }
