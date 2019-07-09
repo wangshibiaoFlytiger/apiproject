5abdf9f17d069bc85eaca4cc78a893de6f79bc6a
wangshibiao <55@qq,com>
2019-07-09 18:10:08 +0800

    refactor(apiproject): 优化redis配置

--- cache/redis.go
+++ cache/redis.go
@@ -6,6 +6,7 @@ import (
 	"github.com/go-redis/redis"
 	"go.uber.org/zap"
 	"strconv"
+	"time"
 )
 
 var RedisClient *redis.Client
@@ -13,10 +14,15 @@ var RedisClient *redis.Client
 // 创建 cache 客户端
 func Init() {
 	RedisClient = redis.NewClient(&redis.Options{
-		Addr:     config.GlobalConfig.RedisHost + ":" + strconv.Itoa(config.GlobalConfig.RedisPort),
-		Password: config.GlobalConfig.RedisPassword,
-		DB:       config.GlobalConfig.RedisDb,
-		PoolSize: config.GlobalConfig.RedisPoolSize,
+		Addr:         config.GlobalConfig.RedisHost + ":" + strconv.Itoa(config.GlobalConfig.RedisPort),
+		Password:     config.GlobalConfig.RedisPassword,
+		DB:           config.GlobalConfig.RedisDb,
+		PoolSize:     config.GlobalConfig.RedisPoolSize,
+		MaxRetries:   2,
+		DialTimeout:  5 * time.Second,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+		MinIdleConns: 50,
 	})
 
 	// 通过 cient.Ping() 来检查是否成功连接到了 cache 服务器
