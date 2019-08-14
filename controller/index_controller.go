ffb575f5d2e3871c45ca1ba9f7fc5603d085e7b3
wangshibiao <55@qq,com>
2019-08-14 15:28:33 +0800

    feat(apiproject): 同步api响应数据加密开关到前端

--- controller/index_controller.go
+++ controller/index_controller.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"apiproject/config"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
@@ -8,6 +9,7 @@ import (
 //渲染首页入口页面
 func Index(ctx *gin.Context) {
 	ctx.HTML(http.StatusOK, "index.html", gin.H{
-		"title": "我是模板变量的内容",
+		"title":   "我是模板变量的内容",
+		"encrypt": config.GlobalConfig.ServiceApiResponseEncrypt,
 	})
 }
