dfa2fed548dc22fc98ac2c9d2c433aac8b77279e
wangshibiao <55@qq,com>
2019-08-14 14:33:46 +0800

    feat(apiproject): 补充base64工具函数

--- util/encrypt_util.go
+++ util/encrypt_util.go
@@ -5,6 +5,13 @@ import "encoding/base64"
 /**
 base64编码
 */
-func Base64Encode(src string) string {
+func Base64EncodeString(src string) string {
 	return base64.StdEncoding.EncodeToString([]byte(src))
+}
+
+/**
+base64编码
+*/
+func Base64EncodeByte(src []byte) string {
+	return base64.StdEncoding.EncodeToString(src)
 }
