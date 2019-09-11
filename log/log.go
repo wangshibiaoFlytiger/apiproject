6421588de122dbd8f6ebc1f5358b7877d8d4615a
wangshibiao <55@qq,com>
2019-09-11 10:52:13 +0800

    refactor(apiproject): 优化日志代码结构

--- log/log.go
+++ log/log.go
@@ -20,45 +20,18 @@ func Init() {
 获取基于lestrrat-go/file-rotatelogs归档的zap logger
 */
 func GetFileRotatelogsLogger() *zap.Logger {
-	//按实际需求灵活定义日志级别
-	infoLevel := zap.LevelEnablerFunc(func(level zapcore.Level) bool {
-		return level <= zapcore.InfoLevel
-	})
-	warnLevel := zap.LevelEnablerFunc(func(level zapcore.Level) bool {
-		return level >= zapcore.WarnLevel
-	})
-
-	infoWriter := getWriter(config.GlobalConfig.LogDir + "info.log")
-	warnWriter := getWriter(config.GlobalConfig.LogDir + "warn.log")
-
-	// 最后创建具体的Logger
-	core := zapcore.NewTee(
-		zapcore.NewCore(
-			// 编码器配置
-			zapcore.NewJSONEncoder(NewEncoderConfig()),
-			// 打印到控制台和文件
-			zapcore.NewMultiWriteSyncer(zapcore.AddSync(os.Stdout),
-				zapcore.AddSync(infoWriter)),
-			// 日志级别
-			infoLevel,
-		),
-		zapcore.NewCore(
-			// 编码器配置
-			zapcore.NewJSONEncoder(NewEncoderConfig()),
-			// 打印到控制台和文件
-			zapcore.NewMultiWriteSyncer(zapcore.AddSync(os.Stdout),
-				zapcore.AddSync(warnWriter)),
-			// 日志级别
-			warnLevel,
-		),
-	)
-
 	// 开启开发模式，堆栈跟踪
 	caller := zap.AddCaller()
 	// 开启文件及行号
 	development := zap.Development()
 	// 设置初始化字段
 	filed := zap.Fields(zap.String("serviceName", "miguo"))
+
+	// 最后创建具体的Logger
+	core := zapcore.NewTee(
+		getCoreList()...,
+	)
+
 	return zap.New(core, caller, development, filed)
 }
 
@@ -83,7 +56,10 @@ func TimeEncoder(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
 	enc.AppendString(t.Format("2006-01-02 15:04:05.000"))
 }
 
-func getWriter(filename string) io.Writer {
+/*
+获取rotatelogs Hook
+*/
+func getRotatelogsHook(filename string) io.Writer {
 	// 生成rotatelogs的Logger 实际生成的文件名 demo.log.YYmmddHH
 	// demo.log是指向最新日志的链接
 	// 保存7天内的日志，每1小时(整点)分割一次日志
@@ -100,4 +76,39 @@ func getWriter(filename string) io.Writer {
 		panic(err)
 	}
 	return hook
+}
+
+func getCoreList() (coreList []zapcore.Core) {
+	//按实际需求灵活定义日志级别
+	infoLevel := zap.LevelEnablerFunc(func(level zapcore.Level) bool {
+		return level <= zapcore.InfoLevel
+	})
+	warnLevel := zap.LevelEnablerFunc(func(level zapcore.Level) bool {
+		return level >= zapcore.WarnLevel
+	})
+
+	rotatelogsInfoHook := getRotatelogsHook(config.GlobalConfig.LogDir + "info.log")
+	rotatelogsWarnHook := getRotatelogsHook(config.GlobalConfig.LogDir + "warn.log")
+
+	coreList = append(coreList,
+		zapcore.NewCore(
+			// 编码器配置
+			zapcore.NewJSONEncoder(NewEncoderConfig()),
+			// 打印到控制台和文件
+			zapcore.NewMultiWriteSyncer(zapcore.AddSync(os.Stdout),
+				zapcore.AddSync(rotatelogsInfoHook)),
+			// 日志级别
+			infoLevel,
+		),
+		zapcore.NewCore(
+			// 编码器配置
+			zapcore.NewJSONEncoder(NewEncoderConfig()),
+			// 打印到控制台和文件
+			zapcore.NewMultiWriteSyncer(zapcore.AddSync(os.Stdout),
+				zapcore.AddSync(rotatelogsWarnHook)),
+			// 日志级别
+			warnLevel,
+		))
+
+	return
 }
