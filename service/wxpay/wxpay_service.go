d8cc04d1c451073396a159c419b7198d8d999069
wangshibiao <55@qq,com>
2019-09-18 11:43:25 +0800

    feat(apiproject): 补充微信支付订单查询方法

--- service/wxpay/wxpay_service.go
+++ service/wxpay/wxpay_service.go
@@ -48,6 +48,39 @@ func (this *WxpayService) WxH5pay(title string, orderNo string, fee int64, userI
 }
 
 /**
+查询微信订单是否支付完成
+*/
+func (this *WxpayService) IsOrderSuccess(orderNo string) (success bool) {
+	//初始化微信客户端
+	//    appId：应用ID
+	//    MchID：商户ID
+	//    apiKey：API秘钥值
+	//    isProd：是否是正式环境
+	client := gopay.NewWeChatClient(config.GlobalConfig.WxpayH5Appid, config.GlobalConfig.WxpayH5Mchid, config.GlobalConfig.WxpayH5Apikey, true)
+
+	//初始化参数结构体
+	body := make(gopay.BodyMap)
+	body.Set("out_trade_no", orderNo)
+	body.Set("nonce_str", gopay.GetRandomString(32))
+	body.Set("sign_type", gopay.SignType_MD5)
+
+	//请求微信订单查询接口
+	wxRsp, err := client.QueryOrder(body)
+	if err != nil {
+		log.Logger.Error("查询微信订单是否支付完成, 异常", zap.Error(err))
+		return false
+	}
+
+	if wxRsp.ResultCode == "SUCCESS" && wxRsp.ReturnCode == "SUCCESS" && wxRsp.ReturnMsg == "OK" && wxRsp.TradeState == "SUCCESS" {
+		log.Logger.Info("查询微信订单是否支付完成, 已支付成功", zap.Any("requestBody", body), zap.Any("responseBody", wxRsp))
+		return true
+	} else {
+		log.Logger.Info("查询微信订单是否支付完成, 非支付成功的订单", zap.Any("requestBody", body), zap.Any("responseBody", wxRsp))
+		return false
+	}
+}
+
+/**
 微信H5支付的回调
 */
 func (this *WxpayService) WxH5payCallback() interface{} {
--- test/wxpay_test.go
+++ test/wxpay_test.go
@@ -7,9 +7,9 @@ import (
 )
 
 /**
-测试微信支付:github.com/iGoogle-ink/gopay
+测试微信支付的统一下单接口:github.com/iGoogle-ink/gopay
 */
-func TestWxpay(t *testing.T) {
+func TestWxpayUnifiedOrder(t *testing.T) {
 	//初始化微信客户端
 	//    appId：应用ID
 	//    MchID：商户ID
@@ -37,4 +37,36 @@ func TestWxpay(t *testing.T) {
 		return
 	}
 	fmt.Println("wxRsp:", *wxRsp)
+}
+
+/**
+测试微信支付查询订单接口
+*/
+func TestWxpayQueryOrder(t *testing.T) {
+	//初始化微信客户端
+	//    appId：应用ID
+	//    MchID：商户ID
+	//    apiKey：API秘钥值
+	//    isProd：是否是正式环境
+	client := gopay.NewWeChatClient("appId", "商户ID", "apikey", true)
+
+	//初始化参数结构体
+	body := make(gopay.BodyMap)
+	body.Set("out_trade_no", "1174152428149096448")
+	body.Set("nonce_str", gopay.GetRandomString(32))
+	body.Set("sign_type", gopay.SignType_MD5)
+
+	//请求订单查询，成功后得到结果
+	wxRsp, err := client.QueryOrder(body)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	if wxRsp.ResultCode == "SUCCESS" && wxRsp.ReturnCode == "SUCCESS" && wxRsp.ReturnMsg == "OK" && wxRsp.TradeState == "SUCCESS" {
+		fmt.Println("支付成功")
+	} else {
+		fmt.Println("非支付成功的订单")
+	}
+	fmt.Println("wxRsp：", *wxRsp)
 }
